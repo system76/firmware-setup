d231034060cc57403c1c5c6921fd26d8859dc2a4
Tim Crawford <tcrawford@system76.com>
2021-02-22 14:47:36 -0700

    deps: Install required depenencies

--- deps.sh
+++ deps.sh
@@ -1,1 +1,12 @@
-sudo apt install texinfo bison flex gperf
+#!/usr/bin/env bash
+
+sudo apt-get install \
+    --yes \
+    make \
+    mtools \
+    parted
+
+# Ensure the correct toolchain is installed
+rustup show
+# Install required components
+rustup component add rust-src
