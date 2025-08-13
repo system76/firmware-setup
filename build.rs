8fe4605bb34186ee6a3bf5cbe435052be84ec9be
Tim Crawford <tcrawford@system76.com>
2025-08-12 21:53:19 -0600

    Use default alignment
    Use default file (0x200) and section (0x1000) alignment.
    
    Fixes the following edk2 warning:
    
        !!!!!!!!  Image Section Alignment(0x20) does not match Required Alignment (0x1000)  !!!!!!!!
    
    Signed-off-by: Tim Crawford <tcrawford@system76.com>

--- build.rs
+++ build.rs
@@ -9,8 +9,6 @@ fn main() {
         println!("cargo::rustc-link-arg=/stack:0,0");
         println!("cargo::rustc-link-arg=/dll");
         println!("cargo::rustc-link-arg=/base:0");
-        println!("cargo::rustc-link-arg=/align:32");
-        println!("cargo::rustc-link-arg=/filealign:32");
         println!("cargo::rustc-link-arg=/subsystem:efi_boot_service_driver");
     }
 }
